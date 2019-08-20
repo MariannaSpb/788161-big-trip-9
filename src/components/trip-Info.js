18bf492ff50d95c4a9b7104297e00b0a686e1a13
Marianna <marry-cherry-pie@mail.ru>
2019-08-21 01:44:02 +0300

    fix date

--- src/components/trip-Info.js
+++ src/components/trip-Info.js
@@ -7,7 +7,7 @@ export const getInfoTrip = () => {
   return `<div class="trip-info__main">
   <h1 class="trip-info__title">${cities.length > 2 ? `${cities[0]} — ... — ${cities[cities.length - 1]}` : `${cities.join(`—`)}`}</h1>
 
-  <p class="trip-info__dates">${dates.length > 2 ? `${new Date(events[0].schedule.date).toLocaleDateString(`en-US`, {day: `numeric`, month: `short`})} — ...${new Date(dates[dates.length - 1]).toLocaleDateString(`en-US`, {day: `numeric`, month: `short`})}` : `${dates.join(`—`)}`}</p>
+  <p class="trip-info__dates">${dates.length > 2 ? `${new Date(events[0].schedule.date).toLocaleDateString(`en-US`, {day: `numeric`, month: `short`})} — ${new Date(dates[dates.length - 1]).toLocaleDateString(`en-US`, {day: `numeric`})}` : `${dates.join(`—`)}`}</p>
 </div>`;
 };
 
